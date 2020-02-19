322fb69c9fde25983b7bc3f585ea37f923ae83cd
Oliver Parkin <langparkin@yahoo.co.uk>
2020-02-19 12:11:55 +1100

    created helper.py, added normalising class

---
+++ helper.py
@@ -0,0 +1,22 @@
+# Helper file containing useful functions and classes for glare detection notebook
+
+
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        if len(tensor.shape) == 3:
+            tensor = tensor.unsqueeze(0)
+        for image in tensor:
+            for t, m, s in zip(image, self.mean, self.std):
+                t.mul_(s).add_(m)
+                # The normalize code -> t.sub_(m).div_(s)
+        return tensor
